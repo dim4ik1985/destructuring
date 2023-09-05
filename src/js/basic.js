6b9d06a75fef8545faece5602924932292d99a78
dim4ik85 <dim4ik1985@yandex.ru>
2023-09-05 22:19:54 +0300

    added files

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,3 +1,14 @@
-export default function multiplication(nums) {
-  return nums.reduce((total, num) => total * num, 1);
+export default function specAttack(obj, destruction = 'Описание недоступно') {
+  const arr = [];
+  // eslint-disable-next-line guard-for-in
+  for (const item in obj.special) {
+    const {
+      id, name, icon, description = destruction,
+    } = obj.special[item];
+    arr.push({
+      id, name, icon, description,
+    });
+  }
+
+  return arr;
 }
